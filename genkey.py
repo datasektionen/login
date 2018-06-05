76b08663c8c9a096185aa05b5c1a36e521f7ce65
tmore <tmore@kth.se>
2018-06-05 22:11:33 +0200

    Added script for generating keys

---
+++ genkey.py
@@ -0,0 +1,19 @@
+#!/bin/bash
+import sys
+from database import Database
+
+
+
+
+if __name__ == "__main__":
+    if len(sys.argv) <= 1:
+        print("Provide a name for the key")
+        exit()
+    if len(sys.argv) > 2:
+        print("Looks like you meant to add some quotes around that brah")
+        exit()
+    key_name = sys.argv[1]
+
+    db = Database()
+    new_key = db.new_api_key(key_name)
+    print("Here's your new key %s \n Store it somewhere safe or look it up in the database when you forget it" % new_key)
