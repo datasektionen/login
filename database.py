4582aad614fca1e840b5ca866b3067bb7856a02f
mattiasgrenfeldt <9297883+mattiasgrenfeldt@users.noreply.github.com>
2020-11-28 18:25:35 +0100

    Cleaned up databse.py using with/as
    Enabled autocommit so that commiting manually isn't needed. https://www.psycopg.org/docs/connection.html\#connection.autocommit
    Used with ... as everwhere a cursor is used. It will automatically close when the with/as-statement is exited.

--- database.py
+++ database.py
@@ -8,122 +8,86 @@ def gen_hash(salt):
     data = secrets.token_bytes(20) + salt.encode("utf-8")
     return hashlib.sha3_256(data).hexdigest()
 
+def gen_api_key(prefix):
+    return prefix + "-" + gen_hash(prefix)
+
 class Database:
     def __init__(self):
         self._connection = psycopg2.connect(os.getenv('DATABASE_URL'))
+        self._connection.autocommit = True
 
-    def commit(self):
-        self._connection.commit()
-
-    def close(self):
+    def __del__(self):
         self._connection.close()
 
     def update_time_created(self, token):
-        cur = self._connection.cursor()
-        query = '''
-        UPDATE tokens
-        SET time_created = NOW()
-        WHERE token = %s
-        '''
-        num_updated = cur.execute(query, (token,))
-        self.commit()
-        cur.close()
-        return num_updated
+        with self._connection.cursor() as cur: 
+            cur.execute('''
+                UPDATE tokens
+                SET time_created = NOW()
+                WHERE token = %s
+            ''', (token,))
 
     def token_by_kthid(self, kthid):
-        cur = self._connection.cursor()
-        query = '''
-        SELECT token
-        FROM tokens
-        WHERE kthid = %s
-        AND time_created > NOW() - interval '1 day'
-        LIMIT 1
-        '''
-        cur.execute(query, (kthid,))
-        res = cur.fetchone()
-        if res:
-            res = res[0]
-        else:
-            res = None
-        cur.close()
-        self.commit()
-        return res
-
-    def kthid_by_token(self, token):
-        cur = self._connection.cursor()
-        query = '''
-        SELECT kthid
-        FROM tokens
-        WHERE token = %s
-        AND time_created > NOW() - interval '1 day'
-        LIMIT 1
-        '''
-        cur.execute(query, (token,))
-        res = cur.fetchone()
-
-        cur.close()
+        with self._connection.cursor() as cur:
+            cur.execute('''
+                SELECT token
+                FROM tokens
+                WHERE kthid = %s
+                AND time_created > NOW() - interval '1 day'
+                LIMIT 1
+            ''', (kthid,))
+            res = cur.fetchone()
         if not res:
             return None
-        self.update_time_created(token)
+        return res[0]
+        
+    def kthid_by_token(self, token):
+        with self._connection.cursor() as cur:
+            cur.execute('''
+                SELECT kthid
+                FROM tokens
+                WHERE token = %s
+                AND time_created > NOW() - interval '1 day'
+                LIMIT 1
+            ''', (token,))
+            res = cur.fetchone()
+        if not res:
+            return None
         return res[0]
 
     def api_key_exists(self, api_key):
-        cur = self._connection.cursor()
-        query = '''
-        SELECT 1
-        FROM api_keys
-        WHERE api_key = %s
-        LIMIT 1
-        '''
-        cur.execute(query, (api_key,))
-        res = cur.fetchone()
-        self.commit()
-        cur.close()
-
+        with self._connection.cursor() as cur:
+            cur.execute('''
+                SELECT 1
+                FROM api_keys
+                WHERE api_key = %s
+                LIMIT 1
+            ''', (api_key,))
+            res = cur.fetchone()
         return not not res
 
     def new_token(self, kthid):
-        cur = self._connection.cursor()
-
         query = '''
-        INSERT INTO tokens (token, kthid, time_created)
-        VALUES (%s, %s, NOW())
+            INSERT INTO tokens (token, kthid, time_created)
+            VALUES (%s, %s, NOW())
         '''
-
-        while cur.rowcount <= 0:
-            new_token = gen_hash(kthid)
-            cur.execute(query, (new_token, kthid))
-
-        self.commit()
-        cur.close()
+        with self._connection.cursor() as cur:
+            # Query will fail if token is a duplicate since it is a primary key.
+            # Retry until it works.
+            while cur.rowcount <= 0:
+                new_token = gen_hash(kthid)
+                cur.execute(query, (new_token, kthid))
         return new_token
 
     def new_api_key(self, prefix):
-        cur = self._connection.cursor()
-
         query = '''
-        INSERT INTO api_keys (api_key, time_created)
-        VALUES (%s, NOW())
+            INSERT INTO api_keys (api_key, time_created)
+            VALUES (%s, NOW())
         '''
-        while cur.rowcount <= 0:
-            key_postfix = gen_hash(prefix)
-            api_key = prefix + "-" + key_postfix
-            cur.execute(query, (api_key,))
-        self.commit()
-        cur.close()
+        with self._connection.cursor() as cur:
+            # Query will fail if api_key is a duplicate since it is a primary
+            # key. Retry until it works.
+            while cur.rowcount <= 0:
+                api_key = gen_api_key(prefix)
+                cur.execute(query, (api_key,))
         return api_key
-
-    # Shitty hack so i dont need to re-generate all api keys
-    def insert_api_key(self, api_key):
-        cur = self._connection.cursor()
-        query = '''
-        INSERT INTO api_keys (api_key, time_created)
-        VALUES (%s, NOW())
-        '''
-        try:
-            cur.execute(query, (api_key,))
-        except:
-            print
-            pass
-        self.commit()
-        cur.close()
