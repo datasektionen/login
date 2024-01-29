033355ea4ac6609d7ddccec1bdbc8446ea5090fa
Lucas Dow <lucdow7@gmail.com>
2024-01-29 14:29:21 +0100

    Update valid_callback regex to include "betasektionen" domain

--- server.py
+++ server.py
@@ -29,7 +29,7 @@ def hello():
 def valid_callback(callback_url):
     if os.environ.get('DONT_VALIDATE_CALLBACK', '0') != '0':
         return True
-    return re.fullmatch("^https?://(([a-zA-Z0-9\-]+[.])*((datasektionen)|(dsekt)|(d-?dagen)|([bm]etaspexet))[.]se|localhost|127\.0\.0\.1)(:[1-9][0-9]*)?/.*$", callback_url) is not None
+    return re.fullmatch("^https?://(([a-zA-Z0-9\-]+[.])*((datasektionen)|(dsekt)|(d-?dagen)|([bm]etaspexet)|(betasektionen))[.]se|localhost|127\.0\.0\.1)(:[1-9][0-9]*)?/.*$", callback_url) is not None
 
 def upgrade_to_https(url):
     if url.startswith("https://"):
